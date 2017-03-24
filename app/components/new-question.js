e05cb150e5be20e61e88f645276198cb52b279fe
Jiwon Kang <jiwonk42@gmail.com>
2017-03-24 12:23:45 -0700

    modify styling on new-question.hbs

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -6,7 +6,6 @@ export default Ember.Component.extend({
     questionFormShow() {
       this.set('addNewQuestion', true);
     },
-
     saveQuestion() {
       var params = {
         title: this.get('title'),
